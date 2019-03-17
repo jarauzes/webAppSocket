fe357c94888a77c2b66253d96c0faea3009c5b1e
Joel Suarez <admin@webtonator.com>
2019-03-17 16:58:08 +0100

    S9 L113: Massage Form & jQuery

---
+++ public/js/index.113.js
@@ -0,0 +1,20 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Connected to server');
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+    console.log('newMessage', message);
+});
+
+socket.emit('createMessage', {
+    from: 'frank',
+    text: 'hi'
+}, function(data) {
+    console.log('Got it!', data);
+});
