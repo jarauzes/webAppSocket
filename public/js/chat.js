449cb232fe2c8189c200021bb0b0ae6eeaa1b0e1
Joel Suarez <admin@webtonator.com>
2019-03-18 17:56:58 +0100

    S9 L121: Adding a join chat Page with room

---
+++ public/js/chat.js
@@ -0,0 +1,77 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('Connected to server');
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').prepend(html);
+
+});
+
+socket.on('newLocationMessage', function (message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template-location').html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').prepend(html);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+
+    e.preventDefault();
+
+    var messageTextbox = jQuery('[name=message]')
+
+    if(jQuery('[name=message]').val().trim() != '') {
+
+        socket.emit('createMessage', {
+            from: 'user',
+            text: jQuery('[name=message]').val()
+        }, function() {
+            messageTextbox.val('');
+        });
+
+        jQuery('[name=message]').val('');
+
+    }
+
+});
+
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Browser Failed with Geolocation!');
+    }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location....');
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location!');
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        locationButton.removeAttr('disabled').text('Send location!');
+        alert('Unable to fetch location.');
+    });
+
+});
