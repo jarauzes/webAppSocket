878fde7fb51e6e3a3e62d39c59295f275aef4359
Joel Suarez <admin@webtonator.com>
2019-03-18 14:54:05 +0100

    S9 L118: Printing message with formated Timestamp

--- playground/time.js
+++ playground/time.js
@@ -1,7 +1,14 @@
 var moment = require('moment');
 
-var date = moment();
+//var date = moment();
 //date.add(100, 'year').subtract(9, 'months');
 //console.log(date.format('MMM Do, YYYY'));
 
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
+var createdAt = 1234;
+
+var date = moment(createdAt);
 console.log(date.format('h:mm a'));
+
